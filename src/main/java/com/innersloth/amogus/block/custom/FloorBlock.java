37c924cc32f798c6e816dc93f748d780528fcdca
maksiurino <153639807+maksiurino@users.noreply.github.com>
2025-10-16 08:51:34 +0200

    Updated

--- src/main/java/com/innersloth/amogus/block/custom/FloorBlock.java
+++ src/main/java/com/innersloth/amogus/block/custom/FloorBlock.java
@@ -3,6 +3,11 @@ package com.innersloth.amogus.block.custom;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class FloorBlock extends Block {
     public static final MapCodec<FloorBlock> CODEC = createCodec(FloorBlock::new);
@@ -14,5 +19,20 @@ public class FloorBlock extends Block {
 
     public FloorBlock(AbstractBlock.Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
+        if (entity instanceof PlayerEntity player) {
+            if (player.isSprinting()) {
+                player.setSprinting(false);
+            }
+
+            if (player.isSneaking()) {
+                player.setSneaking(false);
+            }
+
+            super.onSteppedOn(world, pos, state, entity);
+        }
     }
 }
