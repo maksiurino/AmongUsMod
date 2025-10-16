90b89db5cbbc5c68a909e15a826bab4c9b47774f
maksiurino <153639807+maksiurino@users.noreply.github.com>
2025-10-16 18:47:58 +0200

    Updated

--- src/main/java/com/innersloth/amogus/block/custom/BenchBlock.java
+++ src/main/java/com/innersloth/amogus/block/custom/BenchBlock.java
@@ -1,5 +1,6 @@
 package com.innersloth.amogus.block.custom;
 
+import com.innersloth.amogus.block.custom.enums.BenchPart;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
@@ -7,10 +8,12 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.EnumProperty;
 import org.jetbrains.annotations.Nullable;
 
 public class BenchBlock extends HorizontalFacingBlock {
     public static final MapCodec<BenchBlock> CODEC = createCodec(BenchBlock::new);
+    public static final EnumProperty<BenchPart> BENCH_PART = EnumProperty.of("bench_part", BenchPart.class);
 
     public BenchBlock(AbstractBlock.Settings settings) {
         super(settings);
@@ -23,12 +26,12 @@ public class BenchBlock extends HorizontalFacingBlock {
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
+        builder.add(FACING, BENCH_PART);
     }
 
     @Nullable
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+        return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite()).with(BENCH_PART, BenchPart.EDGES);
     }
 }
