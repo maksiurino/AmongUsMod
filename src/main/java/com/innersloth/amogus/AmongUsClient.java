559c9f619dca27030993654421e9ab9e7ffa2f69
maksiurino <153639807+maksiurino@users.noreply.github.com>
2025-10-18 12:47:21 +0200

    Updated

--- src/main/java/com/innersloth/amogus/AmongUsClient.java
+++ src/main/java/com/innersloth/amogus/AmongUsClient.java
@@ -7,6 +7,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.util.Window;
 
 public class AmongUsClient implements ClientModInitializer {
     @Override
