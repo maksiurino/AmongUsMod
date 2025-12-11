d334232a8fd58c2412aea9d68b1e6673a406cf90
maksiurino <153639807+maksiurino@users.noreply.github.com>
2025-12-11 18:50:56 +0100

    Updated

--- src/main/java/com/innersloth/amogus/config/AmongUsConfig.java
+++ src/main/java/com/innersloth/amogus/config/AmongUsConfig.java
@@ -2,8 +2,10 @@ package com.innersloth.amogus.config;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = "among-us")
 public class AmongUsConfig implements ConfigData {
-    boolean toggleA = false;
+    @ConfigEntry.ColorPicker
+    int player_color = 0xFFFFFF;
 }
